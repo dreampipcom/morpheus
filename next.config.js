9339fdb928417cac99dd2ec4c8f3a9e8fe538b1f
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2024-08-07 18:40:59 +0100

    ar(fix) [DPCP-62]: Consolidate Domain

--- next.config.js
+++ next.config.js
@@ -138,10 +138,14 @@ const config = {
             {"source": "/services/:match*", "destination": "https://nyx.dreampip.com/dash/services/:match*"},
             {"source": "/app", "destination": "https://alpha.dreampip.com/"},
             {"source": "/app/:match*", "destination": "https://alpha.dreampip.com/:match*"},
-            {
-                source: '/subscribe',
-                destination: 'http://store.dreampip.com/',                
-            },
+            // {
+            //     source: '/subscribe',
+            //     destination: 'https://members.dreampip.com/',                
+            // },
+            // {
+            //     source: '/members',
+            //     destination: 'https://members.dreampip.com/',                
+            // },
             {
                 source: '/members/calendar',
                 destination: 'https://chat.workspace.dreampip.com',                
@@ -160,75 +164,11 @@ const config = {
             },
             {
                 source: '/api/nexus/audio/0',
-                destination: 'https://radio.media.infra.dreampip.com/0',
-            },
+                destination: 'http://207.246.121.205/0',
+            },            
             {
                 source: '/api/nexus/audio/1',
-                destination: 'http://66.135.5.207/0',
-            },
-            {
-                source: '/api/nexus/audio/2',
-                destination: 'http://66.135.5.207/1',
-            },
-            {
-                source: '/api/nexus/audio/4',
-                destination: 'https://radio.media.infra.dreampip.com/3',
-            },
-            {
-                source: '/api/nexus/audio/5',
-                destination: 'https://radio.media.infra.dreampip.com/4',
-            },
-            {
-                source: '/api/nexus/audio/6',
-                destination: 'https://radio.media.infra.dreampip.com/5',
-            },
-            {
-                source: '/api/nexus/audio/7',
-                destination: 'https://radio.media.infra.dreampip.com/6',
-            },
-            {
-                source: '/api/nexus/audio/8',
-                destination: 'https://radio.media.infra.dreampip.com/7',
-            },
-            {
-                source: '/api/nexus/audio/9',
-                destination: 'https://radio.media.infra.dreampip.com/8',
-            },
-            {
-                source: '/api/nexus/audio/10',
-                destination: 'https://radio.media.infra.dreampip.com/9',
-            },
-            {
-                source: '/api/nexus/audio/11',
-                destination: 'https://radio.media.infra.dreampip.com/10',
-            },
-            {
-                source: '/api/nexus/audio/12',
-                destination: 'https://radio.media.infra.dreampip.com/12',
-            },
-            {
-                source: '/api/nexus/audio/13',
-                destination: 'https://radio.media.infra.dreampip.com/12',
-            },
-            {
-                source: '/api/nexus/audio/14',
-                destination: 'https://radio.media.infra.dreampip.com/13',
-            },
-            {
-                source: '/api/nexus/audio/15',
-                destination: 'https://radio.media.infra.dreampip.com/14',
-            },
-            {
-                source: '/api/nexus/audio/16',
-                destination: 'https://radio.media.infra.dreampip.com/15',
-            },
-            {
-                source: '/api/nexus/audio/17',
-                destination: 'https://radio.media.infra.dreampip.com/16',
-            },
-            {
-                source: '/api/nexus/audio/3',
-                destination: 'http://66.135.5.207/1',
+                destination: 'http://207.246.121.205/1',
             },
         ]
     }
