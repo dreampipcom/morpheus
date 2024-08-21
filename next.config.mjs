529764a21722adb2beb04520c9eea50b730180d6
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2024-08-21 12:12:31 +0100

    ar(feat) [DPCP-37]: Localization

--- next.config.mjs
+++ next.config.mjs
@@ -139,8 +139,10 @@ const config = {
     },
     async rewrites() {
         return [
-            {"source": "/dash", "destination": process.env.NEXUS_HOST},
-            {"source": "/dash/:match*", "destination": `${process.env.NEXUS_HOST}/dash/:match*`},
+            {"source": "/dash", "destination": process.env.NEXUS_HOST, locale: false},
+            {"source": "/dash/:match*", "destination": `${process.env.NEXUS_HOST}/dash/:match*`, locale: false},
+            {"source": "/:locale/dash", "destination": `${process.env.NEXUS_HOST}/:locale`, locale: false},
+            {"source": "/:locale/dash/:match*", "destination": `${process.env.NEXUS_HOST}/:locale/dash/:match*`, locale: false},
             {"source": "/api/v1", "destination": `${process.env.NEXUS_HOST}/api/v1`},
             {"source": "/api/v1/:match*", "destination": `${process.env.NEXUS_HOST}/api/v1/:match*`},
             {"source": "/services", "destination": `${process.env.NEXUS_HOST}/dash/services`},
